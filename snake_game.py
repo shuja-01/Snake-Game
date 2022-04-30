a713bcc991c2c7a1a85e2653bedc1332407ac2f1
Shuja <mshuja.rizvi@gmail.com>
2022-04-30 13:22:00 +0530

    modified

--- snake_game.py
+++ snake_game.py
@@ -3,17 +3,15 @@ import random
 
 pygame.init()
 
-# Colors
+
 white = (255, 255, 255)
 red = (255, 0, 0)
 black = (0, 0, 0)
 
-# Creating window
 screen_width = 1900
 screen_height = 1010
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 
-# Game Title
 pygame.display.set_caption("Snakes")
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -46,9 +44,7 @@ def welcome():
         clock.tick(60)
 
 
-# Game Loop
 def gameloop():
-    # Game specific variables
     exit_game = False
     game_over = False
     snake_x = 45
